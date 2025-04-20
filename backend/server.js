f529defbc58ddc065edb8a408c00986565ca8834
Eman Furrukh <emanfurrukh09@gmail.com>
2025-04-20 12:28:38 +0500

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+/* backend file*/
+
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
@@ -19,3 +21,4 @@ app.use('/api', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
